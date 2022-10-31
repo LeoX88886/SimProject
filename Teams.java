421fc30ce4ad1ddaadb0268b483b48cd5c456a9f
TysonPellatt <350019410@gapps.yrdsb.ca>
2022-10-31 14:39:48 -0400

    team sizes is 30 by 30

--- Teams.java
+++ Teams.java
@@ -17,7 +17,7 @@ public class Teams extends Actor
     
     public Teams()
     {
-        teamImage.scale(100,100);
+        teamImage.scale(30,30);
         setImage(teamImage);
     }
     public void act()
