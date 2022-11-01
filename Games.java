41094d4ba2eaefee5ba1f6196cc5e485d47fa18e
Xu <101126520+LeoX88886@users.noreply.github.com>
2022-11-01 14:26:28 -0400

    added score label and mouse location tracker

--- Games.java
+++ Games.java
@@ -12,18 +12,61 @@ public class Games extends Actor
      * Act - do whatever the Games wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    
+    //Game console icon
     private GreenfootImage gameImage = new GreenfootImage("images/GamesIcon/game.png");
+    
+    //instance of scores
+    private static int scoreOne;
+    private static int scoreTwo;
+    
     
     public Games()
     {
         //gameImage.scale(80,80);
         setImage(gameImage);
         
+        scoreOne = 0;
+        scoreTwo = 0;
+        
+        
 
     }
+    
     public void act()
     {
         // Add your action code here.
+        if(Greenfoot.isKeyDown("1"))
+        {
+            scoreOne++;
+        }
+        
+        if(Greenfoot.isKeyDown("2"))
+        {
+            scoreTwo++;
+        }
+        
+        if(Greenfoot.isKeyDown("3"))
+        {
+            scoreOne--;
+        }
+        
+        if(Greenfoot.isKeyDown("4"))
+        {
+            scoreTwo++;
+        }
     }
+    
+    
+    public static int getScoreOne()
+    {
+        return scoreOne;
+    }
+    
+    public static int getScoreTwo()
+    {
+        return scoreTwo;
+    }
+    
+    
+    
 }
--- MyWorld.java
+++ MyWorld.java
@@ -15,9 +15,11 @@ public class MyWorld extends World
      */
     private static GreenfootImage background = new GreenfootImage ("images/pacMan Maze.png");
     
+    //instance of the classes
     Effects e = new Effects();
     Games g = new Games();
     Teams t = new Teams();
+    
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
@@ -26,11 +28,33 @@ public class MyWorld extends World
         background.scale(1024, 800);
         setBackground (background);
         
+        
+        
+        
         addObject(e, 100, 100);
         addObject(g, 100, 200);
         addObject(t, 100, 300);
+        
+        
 
 
         
     }
+    
+    public void act()
+    {
+        MouseInfo m = Greenfoot.getMouseInfo();
+        if (m != null)
+        {
+            showText("X: " + String.valueOf(m.getX()), 200, 200);
+            showText("Y: " + String.valueOf(m.getY()), 300, 200);
+        }
+        
+        
+        showText("team 1 score: " + Games.getScoreOne(), 300, 50);
+        showText("team 2 score: " + Games.getScoreTwo(), 500, 50);
+        
+        
+    }
+    
 }
