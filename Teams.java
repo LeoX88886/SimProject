9c0a190e0db496e81f45964aa66775edeb5b25ea
TysonPellatt <350019410@gapps.yrdsb.ca>
2022-11-02 14:44:01 -0400

    random movement direction switch code.
    the game breaks when at world edge.

--- Teams.java
+++ Teams.java
@@ -18,7 +18,10 @@ public class Teams extends Actor
     Color wall = new Color(9,0,255);
     protected int directionR;// 1 = right, -1 = left
     protected int directionL;
+    protected int directionU;
+    protected int directionD;
     protected int speed;
+    private int f = 1;
     //Teams t = (Teams)getOneObjectAtOffset(0,0,Teams.class);
     public Teams()
     {
@@ -33,15 +36,28 @@ public class Teams extends Actor
     {
         // Add your action code here.
         Walk();
+        WallCollison();
     }
     public void Walk()
     {
+        if(f == 1){
+            setLocation(getX()+1,getY());
+        }else if(f == 2){
+            setLocation(getX()-1,getY());
+        }else if(f == 3){
+            setLocation(getX(),getY()-1);
+        }else if(f == 4){
+            setLocation(getX(),getY()+1);
+        }
+        //setLocation(getX()+1,getY());
+    }
+    public void WallCollison(){
         directionR = 1;
-        int f = 1;
         directionL = -1;
-        speed = 1;
-        int shift = Greenfoot.getRandomNumber(3);
+        directionU = -1;
+        directionD = 1;
         
+        int shift = Greenfoot.getRandomNumber(3);
         Color right = (getWorld().getBackground().getColorAt(getX() + getImage().getWidth()/2,getY()));
         Color left = (getWorld().getBackground().getColorAt(getX() + getImage().getWidth()/-2,getY()));
         Color up = (getWorld().getBackground().getColorAt(getX(),getY() + getImage().getHeight()/-2));
@@ -52,16 +68,16 @@ public class Teams extends Actor
             if(shift == 1)
             {
                 f = 2;
-                //shift = Greenfoot.getRandomNumber (3);
+                setLocation(getX()-5,getY());
             }else if(shift == 2)
             {
                 f = 3;
-                //shift = Greenfoot.getRandomNumber (3);
+                setLocation(getX()-5,getY());
             }
             if(shift == 3)
             {
                 f = 4;
-                //shift = Greenfoot.getRandomNumber (3);
+                setLocation(getX()-5,getY());
             }
         }
         else if((left.getRed() == wall.getRed() && left.getGreen() == wall.getGreen() && left.getBlue() == wall.getBlue()) && directionL == -1)
@@ -70,72 +86,56 @@ public class Teams extends Actor
             if(shift == 1)
             {
                 f = 1;
-                //shift = Greenfoot.getRandomNumber (3);
+                setLocation(getX()+5,getY());
             }else if(shift == 2)
             {
                 f = 3;
-                //shift = Greenfoot.getRandomNumber (3);
+                setLocation(getX()+5,getY());
             }
             if(shift == 3)
             {
                 f = 4;
-                //shift = Greenfoot.getRandomNumber (3);
+                setLocation(getX()+5,getY());
             }
         }
-        else if((up.getRed() == wall.getRed() && up.getGreen() == wall.getGreen() && up.getBlue() == wall.getBlue()) && directionL == -1)
+        else if((up.getRed() == wall.getRed() && up.getGreen() == wall.getGreen() && up.getBlue() == wall.getBlue()) && directionU == -1)
         {
             shift = Greenfoot.getRandomNumber(3);
             if(shift == 1)
             {
                 f = 1;
-                //shift = Greenfoot.getRandomNumber (3);
+                setLocation(getX(),getY()-5);
             }else if(shift == 2)
             {
                 f = 2;
-                //shift = Greenfoot.getRandomNumber (3);
+                setLocation(getX(),getY()-5);
             }
             if(shift == 3)
             {
                 f = 4;
-                //shift = Greenfoot.getRandomNumber (3);
+                setLocation(getX(),getY()-5);
             }
         }
-        else if((down.getRed() == wall.getRed() && down.getGreen() == wall.getGreen() && down.getBlue() == wall.getBlue()) && directionR == 1)
+        else if((down.getRed() == wall.getRed() && down.getGreen() == wall.getGreen() && down.getBlue() == wall.getBlue()) && directionD == 1)
         {
             shift = Greenfoot.getRandomNumber(3);
             if(shift == 1)
             {
                 f = 1;
-                //shift = Greenfoot.getRandomNumber (3);
+                setLocation(getX(),getY()+5);
             }else if(shift == 2)
             {
                 f = 2;
-                //shift = Greenfoot.getRandomNumber (3);
+                setLocation(getX(),getY()+5);
             }
             if(shift == 3)
             {
                 f = 3;
-                //shift = Greenfoot.getRandomNumber (3);
+                setLocation(getX(),getY()+5);
             }
-        }
-        else{
-            //move(speed * direction);
-            if(f == 1)
-            {
-                setLocation(getX()+1,getY());
-            }else if(f == 2)
-            {
-                setLocation(getX()-1,getY());
-            }else if(f == 3)
-            {
-                setLocation(getX(),getY()-1);
-            }else if(f == 4)
-            {
-                setLocation(getX(),getY()+1);
-            }
-            //setLocation(getX()+1,getY());
         }
     }
+
     /**
      * if(img.getColorAt(getX(),getY()) == color
      * {
