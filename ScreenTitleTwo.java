4dd4bf7f3ecdc7b7e0d121cb509995b10c9843db
LeoX88886 <101126520+LeoX88886@users.noreply.github.com>
2022-11-22 18:52:56 -0500

    changed screenTitle

---
+++ ScreenTitleTwo.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ScreenTitle here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ScreenTitleTwo extends Actor
+{
+    /**
+     * Act - do whatever the ScreenTitle wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    private GreenfootImage ScreenImage = new GreenfootImage("images/screenTitle.png");
+    public ScreenTitleTwo()
+    {
+        
+        
+        
+        setImage(ScreenImage);
+    }
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
